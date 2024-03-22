eaf38ce0352a716bf5c2d6f7fa6c8b4fcdfd42a5
김희수 <up4025@naver.com>
2024-03-22 09:58:30 +0900

    Update models.py

--- pybo/models.py
+++ pybo/models.py
@@ -19,7 +19,7 @@ class Answer(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author_answer')
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     content = models.TextField()
-    create_date = models.DateTimeField(null=True, blank=True)
+    create_date = models.DateTimeField()
     modify_date = models.DateTimeField(null=True, blank=True)
     voter = models.ManyToManyField(User, related_name='voter_answer')
     oppose = models.ManyToManyField(User, related_name='oppose_answer')
