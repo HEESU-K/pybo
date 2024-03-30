fac340d021d7fb6cc6d6a36afc220f2bb61b8bc5
김희수 <up4025@naver.com>
2024-03-30 14:05:34 +0900

    Update views.py

--- common/views.py
+++ common/views.py
@@ -16,8 +16,3 @@ def signup(request):
     else:
         form = UserForm()
     return render(request, 'common/signup.html', {'form': form})
-
-
-def logout_view(request):
-    logout(request)
-    return redirect('index')
